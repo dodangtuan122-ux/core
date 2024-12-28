11b14c0a071755e9a0ef3133e7be1e9a19961811
DevForge AI <dodangtuan122@gmail.com>
2024-12-28 09:00:00 +0000

    refactor: improve workflow context sharing between agents

---
+++ src/orchestrator/context.py
@@ -0,0 +1,11 @@
+"""Shared context between agents."""
+
+class AgentContext:
+    def __init__(self):
+        self.data = {}
+    
+    def set(self, key, value):
+        self.data[key] = value
+    
+    def get(self, key, default=None):
+        return self.data.get(key, default)
